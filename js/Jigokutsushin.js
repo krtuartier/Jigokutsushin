fbc65e4812b7a74a176d3505571d79fd4d2c6266
krtuartier <krtuartier@gmail.com>
2017-07-29 12:04:47 +0800

    updata

--- js/Jigokutsushin.js
+++ js/Jigokutsushin.js
@@ -15,17 +15,13 @@ function weeHours(){
 	if ((now.getHours()==0&&now.getMinutes()<=10)||url.indexOf('?0')>=0) {
 		document.body.style.backgroundColor='#000000';
 		document.body.style.color='#FFFFFF';
-		document.body.innerHTML='<video id="vd" controls="controls"><source src="img/fire.mp4" type="video/mp4"></source></video>';
-		var oVd=document.getElementById('vd');
-		oVd.play();
-		var handler=setInterval(function(){
-			if (oVd.ended) {
-				document.body.removeChild(oVd);
-				document.body.innerHTML='<div class="margintop"></div><form role="form"><p class="msg">あなたの怨み、晴らします。</p><div class="form_wrap form-group"><input class="txt form-control" type="text"/></div><div class="form_wrap form-group"><input class="btn" type="button" value="送 信" /></div></form>';
-				document.querySelector('.txt').focus();
-				clearInterval(handler);
-			}
-		},1);
+		document.body.innerHTML='<img class="op" src="img/fire.gif"/>';
+		var oOp=document.querySelector('.op');
+		setTimeout(function(){
+			document.body.removeChild(oOp);
+			document.body.innerHTML='<div class="margintop"></div><form role="form"><p class="msg">あなたの怨み、晴らします。</p><div class="form_wrap form-group"><input class="txt form-control" type="text"/></div><div class="form_wrap form-group"><input class="btn" type="button" value="送 信" /></div></form>';
+			document.querySelector('.txt').focus();
+		},2000);
 	} else{
 		document.body.style.backgroundColor='#FFFFFF';
 		document.body.style.color='#000000';
