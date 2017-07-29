65637da0fe75ed61823d8f92b9bea4567027a4c6
krtuartier <krtuartier@gmail.com>
2017-07-29 12:07:37 +0800

    updata

--- js/Jigokutsushin.js
+++ js/Jigokutsushin.js
@@ -8,31 +8,100 @@ function RootNodeFont(){
 		this.hWidth=this.html.getBoundingClientRect().width;
 		this.html.style.fontSize=this.hWidth/30+"px";
 	}
-} 
-function weeHours(){
-	var now=new Date();
-	var url=window.location+'';
-	if ((now.getHours()==0&&now.getMinutes()<=10)||url.indexOf('?0')>=0) {
-		document.body.style.backgroundColor='#000000';
-		document.body.style.color='#FFFFFF';
-		document.body.innerHTML='<video id="vd" controls="controls"><source src="img/fire.mp4" type="video/mp4"></source></video>';
-		var oVd=document.getElementById('vd');
-		oVd.play();
-		var handler=setInterval(function(){
-			if (oVd.ended) {
-				document.body.removeChild(oVd);
-				document.body.innerHTML='<div class="margintop"></div><form role="form"><p class="msg">あなたの怨み、晴らします。</p><div class="form_wrap form-group"><input class="txt form-control" type="text"/></div><div class="form_wrap form-group"><input class="btn" type="button" value="送 信" /></div></form>';
-				document.querySelector('.txt').focus();
-				clearInterval(handler);
-			}
-		},1);
-	} else{
-		document.body.style.backgroundColor='#FFFFFF';
-		document.body.style.color='#000000';
-		document.body.innerHTML='<div class="margintop"></div><div class="container not_found_404"><h2>Not Found</h2><p>The request URL was not found on this server.<br />hellish world/1.3.33 Server at Jigokutsushin_net Port 80</p></div>';
-	}
 }
 window.onload=function(){
-	weeHours();
 	RootNodeFont();
+	var Op={
+		template:'<img class="op" src="img/fire.gif"/>',
+		created(){
+			document.body.style.backgroundColor='#000000';
+			document.body.style.color='#FFFFFF';
+			setTimeout(function(){
+				router.replace({path:'Jigokutsushin'});
+			},2000);
+		}
+	};
+	var Jigokutsushin={
+		template:'<form role="form"><p class="msg">あなたの怨み、晴らします。</p><div class="form_wrap form-group"><input class="txt form-control" type="text"/></div><div class="form_wrap form-group"><router-link to="/loading" class="btn btn-default">送 信</router-link></router-link></div></form>',
+		mounted(){
+			document.body.style.backgroundColor='#000000';
+			document.body.style.color='#FFFFFF';
+			this.$el.children[1].children[0].focus();
+		}
+	};
+	var Loading = {
+		template: '<div class="loading"><i class="glyphicon glyphicon-hourglass"></i></div>',
+		mounted(){
+			document.body.style.backgroundColor='#000000';
+			document.body.style.color='#FFFFFF';
+			setTimeout(function(){
+				router.replace({path:'success'});
+			},6000);
+		}
+	};
+	var Success={
+		template:'<div class="success"><p><i class="glyphicon glyphicon-envelope"></i></p><p>地獄通信</p></div>',
+		mounted(){
+			document.body.style.backgroundColor='#ff6d81';
+			document.body.style.color='#000000';
+			setTimeout(function(){
+				router.replace({path:'receive'});
+			},3000);
+		}
+	};
+	var Receive={
+		template:'<div class="receive"><p>受け取りました</p><p>地獄少女</p></div>',
+		mounted(){
+			document.body.style.backgroundColor='#ff6d81';
+			document.body.style.color='#000000';
+		}
+	};
+	var Notfound404={
+		template:'<div class="container not_found_404"><h2>Not Found</h2><p>The request URL was not found on this server.<br />hellish world/1.3.33 Server at Jigokutsushin_net Port 80</p></div>',
+		created(){
+			var now=new Date();
+			if(window.location.search=='?0'||(now.getHours()==0&&now.getMinutes()<=10)){
+				router.replace({path:'op'});
+			}
+		}
+	};
+	const routes = [
+		{
+			path:'/op',
+			component:Op
+		},
+		{
+			path:'/jigokutsushin',
+			component:Jigokutsushin
+		},
+		{
+			path: '/loading',
+			component: Loading
+		},
+		{
+			path: '/success',
+			component: Success
+		},
+		{
+			path: '/receive',
+			component: Receive
+		},
+		{
+			path:'*',
+			redirect: '/notfound404'
+		},
+		{
+			path:'/notfound404',
+			component:Notfound404
+		}
+	];
+	const router = new VueRouter({
+//		mode: 'history',
+//		base: __dirname,
+		routes
+	});
+	new Vue({
+		el: '#box',
+		router
+	});
 }
